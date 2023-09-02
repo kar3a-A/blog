d20db7c5efcb3618aa510ce85b94d9953fcf7774
kar3a-A <kkh2002csw@gmail.com>
2023-09-02 21:56:07 +0630

    navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom";
 
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+const Navbar = () => (
+  <header>
+    <nav>
+      <h2 className="nav-title">Blog</h2>
+
+      <ul className="nav-ul">
+      <Link to='/'>
+        <li>Home</li>
+      </Link>
+      <Link to='/about'>
+        <li>About</li>
+      </Link>
+      <Link to='blog'>
+        <li>Blog</li>
+      </Link>
+      </ul>
+    </nav>
+  </header>
+);
+
+
 
 export default Navbar
